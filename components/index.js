f8ed45bfac2706daae530341bc04a1f9228a7b94
Hamza-SE <muhammadhamza007123@gmail.com>
2022-11-16 02:32:28 +0500

    first commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+
+export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton, DetailsDesc, DetailsBid };
---
+++ screens/Details.js
@@ -0,0 +1,15 @@
+import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
+
+import { COLORS, FONTS, SIZES, SHADOWS, assets } from '../constants'
+import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid } from "../components";
+
+
+const Details = () => {
+    return (
+        <View>
+            <Text>Details</Text>
+        </View>
+    )
+}
+
+export default Details
